593f925218d511ab02826645f608011201a44af9
clojj <jwin1968@gmail.com>
2016-06-20 11:22:27 +0200

    refine rpc test

--- rpc-intero.js
+++ rpc-intero.js
@@ -4,6 +4,7 @@ var Session = require('msgpack5rpc');
 var socket = net.connect({ path: "/Users/jwin/stack-projects/ghc8-playground/.intero.sock" }, function () {
   session = new Session();
   session.attach(socket, socket);
+  
   session.request("type", ["someFunc"], function (err, res) {
     if (err) {
       console.error(err);
@@ -11,5 +12,14 @@ var socket = net.connect({ path: "/Users/jwin/stack-projects/ghc8-playground/.in
     }
 
     console.log(res);
+  });
+  
+  session.request("uses-at", ["Main", 10, 1, 10, 1, "startState"], function (err, res) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    console.log(JSON.stringify(res));
   });
 });
